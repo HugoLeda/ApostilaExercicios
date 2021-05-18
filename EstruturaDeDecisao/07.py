73c7bd0122856fd3551a2929a4577f46d60af69e
João Hugo Leda <74215171+HugoLeda@users.noreply.github.com>
2021-05-18 20:44:25 -0300

    Update 07.py

--- EstruturaDeDecisao/07.py
+++ EstruturaDeDecisao/07.py
@@ -5,4 +5,4 @@ numeros = []
 for i in range (3) :
   numeros.append(float(input("Digite um número: ")))
 
-print(min(numeros, key = float)) # max restorna o maior valor de uma lista
+print(min(numeros, key = float)) # mix restorna o menor valor de uma lista
