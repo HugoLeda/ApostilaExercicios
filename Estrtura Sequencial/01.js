ac8a8400d75f1a4427e3791d707a4382119dfe03
João Hugo Leda <joaohugoleda2@gmail.com>
2022-08-03 23:10:57 -0300

    estrtura sequencial

---
+++ Estrtura Sequencial/01.js
@@ -0,0 +1,7 @@
+/*
+Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+*/
+
+const aloMundo = () => console.log("hello world")
+
+aloMundo()
---
+++ Estrtura Sequencial/06.js
@@ -0,0 +1,7 @@
+/*
+Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+*/
+
+const areaCirculo = (raio) => Math.PI * (raio * raio)
+
+console.log(areaCirculo(2))
