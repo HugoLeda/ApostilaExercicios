283d9b22597bd5b741911c7c3de68c8778a80568
João Hugo Leda <joaohugoleda2@gmail.com>
2023-01-07 20:49:41 -0300

    01 JS estrutura de decisão

---
+++ Estrutura de Decisao/01.js
@@ -0,0 +1,11 @@
+n1 = Number(input("Digite um número: "));
+n2 = Number(input("Digite um número: "));
+
+
+if (n1 > n2) {
+  console.log('Maior', n1);
+} else if (n2 > n1) {
+  console.log('Maior', n2);
+} else {
+  console.log('Números iguais');
+}
