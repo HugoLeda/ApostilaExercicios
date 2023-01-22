1718216a17725d0bae43ed8b592587caa43a4ff4
João Hugo Leda <joaohugoleda2@gmail.com>
2023-01-21 23:06:45 -0300

    09 sequencial

---
+++ Estrtura Sequencial/09.js
@@ -0,0 +1,8 @@
+/* 
+Faça um Programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius.
+  C = (5 * (F-­32) / 9).
+*/
+
+function farenheitToCelsius(temperatura) {
+  return (5 * (temperatura - 32)) / 9
+}
